a5c917823d3a2b2480deb7f7daf1a3be2e280271
roloe25 <for.aljo@gmail.com>
2020-02-27 09:45:19 -0800

    created a dbase connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,18 @@
 var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "student",
+  password: "student",
+  database: "chat"
+});
+
+con.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected!");
+});
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
