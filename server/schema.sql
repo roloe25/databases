b8b4a4037c9b010b3e13a1fef7185c8d6694be56
roloe25 <for.aljo@gmail.com>
2020-02-26 17:41:23 -0800

    started defining tables in sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  userid INT NOT NULL,
+  text VARCHAR(120),
+  roomid INT NOT NULL,
+  FOREIGN KEY (userid), REFERENCES (users(userid)),
+  FOREIGN KEY (roomid), REFERENCES (rooms(roomid))
 );
+
+CREATE TABLE users (
+  userid INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20),
+  PRIMARY KEY (userid)
+);
+
+CREATE TABLE rooms (
+  roomid INT NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(20),
+  PRIMARY KEY (roomid)
+);
+
+
 
 /* Create other tables and define schemas for them here! */
 
